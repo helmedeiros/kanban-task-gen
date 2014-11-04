78b9f2b0cb49b778375a634555173ff00c31aa50
Henrique Oliveira <henrique.so@gmail.com>
2014-11-04 20:38:37 -0200

    Authenticate with anonymous login

--- js/kanban-task-gen-1-1.js
+++ js/kanban-task-gen-1-1.js
@@ -220,7 +220,9 @@ console.log('Kanban Task Gen v1.1');
         
         // If no current user authenticate anonymously
         if (!user) {
-            //authAnonymously();
+            
+            authAnonymously();
+            
             // pop up error
             showAlert({
                 title: '',
