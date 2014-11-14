e6f1905af95438b94b8eccc3708015bdcb5ca7dd
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2014-11-14 20:09:51 -0200

    Cover buildPostIt with spec

--- test/spec/KanbanTaskGenSpec.js
+++ test/spec/KanbanTaskGenSpec.js
@@ -14,4 +14,40 @@ describe("Page", function() {
     expect($.ajax.calls.mostRecent().args[0].url).toEqual(configuration.url);
   });
 
+  describe("buildPostIt", function() {
+    var sampleStory;
+
+    beforeEach(function() {
+      sampleStory = {
+        id: "42",
+        priority: "1",
+        name: "Sample story",
+        specialist1: "BE",
+        time1: "3",
+        specialist2: "FE",
+        time2: "2",
+        sprint: "7"
+      };
+    });
+
+    it("returns a .post-it element", function() {
+      expect(page.buildPostIt(sampleStory).hasClass("post-it")).toBe(true);
+    });
+
+    it("renders id, priority, name and sprint from the story", function() {
+      var postIt = page.buildPostIt(sampleStory);
+      expect(postIt.find(".id").text()).toEqual(sampleStory.id);
+      expect(postIt.find(".priority").text()).toEqual(sampleStory.priority);
+      expect(postIt.find(".name").text()).toEqual(sampleStory.name);
+      expect(postIt.find(".sprint").text()).toEqual(sampleStory.sprint);
+    });
+
+    it("renders both specialities", function() {
+      var specialities = page.buildPostIt(sampleStory).find(".speciality");
+      expect(specialities.length).toEqual(2);
+      expect($(specialities[0]).text()).toEqual(sampleStory.specialist1);
+      expect($(specialities[1]).text()).toEqual(sampleStory.specialist2);
+    });
+  });
+
 });
