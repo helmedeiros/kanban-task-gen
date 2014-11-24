5fcce8a2c2eead7e8dcf73cd4ebf3935fe41fc57
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2014-11-24 21:11:37 -0200

    Introduce AuthService

---
+++ js/AuthService.js
@@ -0,0 +1,57 @@
+function AuthService(rootRef) {
+    this.rootRef = rootRef;
+}
+
+AuthService.prototype = {
+
+    signInWith: function(provider) {
+        var deferred = $.Deferred();
+        this.rootRef.authWithOAuthPopup(provider, function(err, user) {
+            if (err) {
+                deferred.reject(err);
+            }
+            if (user) {
+                deferred.resolve(user);
+            }
+        });
+        return deferred.promise();
+    },
+
+    signInWithPassword: function(credentials) {
+        var deferred = $.Deferred();
+        this.rootRef.authWithPassword(credentials, function(err, user) {
+            if (err) {
+                deferred.reject(err);
+            }
+            if (user) {
+                deferred.resolve(user);
+            }
+        });
+        return deferred.promise();
+    },
+
+    signInAnonymously: function() {
+        var deferred = $.Deferred();
+        this.rootRef.authAnonymously(function(err, authData) {
+            if (authData) {
+                deferred.resolve(authData);
+            }
+            if (err) {
+                deferred.reject(err);
+            }
+        });
+        return deferred.promise();
+    },
+
+    createUser: function(credentials) {
+        var deferred = $.Deferred();
+        this.rootRef.createUser(credentials, function(err) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve();
+            }
+        });
+        return deferred.promise();
+    }
+};
