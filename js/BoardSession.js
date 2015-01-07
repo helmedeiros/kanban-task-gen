74b4086573c68c975807f30ff20f29119f45ab7e
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-01-07 18:24:18 -0200

    Introduce BoardSession

---
+++ js/BoardSession.js
@@ -0,0 +1,18 @@
+function BoardSession(deps) {
+    this.userRefFor = deps.userRefFor;
+    this.page = deps.page;
+    this.counter = deps.counter;
+    this.target = deps.target;
+    this.repository = null;
+}
+
+BoardSession.prototype.start = function(authData) {
+    this.target.empty();
+    this.repository = new BoardRepository(this.userRefFor(authData));
+    var page = this.page;
+    var counter = this.counter;
+    this.repository.onCardAdded(function(data) {
+        counter.observe(data.id);
+        page.parseStories({ tasks: [data] });
+    });
+};
