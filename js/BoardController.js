f5d987bca16e4ef74bbfe5163b02e517a71887cf
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-01-21 18:24:18 -0200

    Show card counts on board column titles

--- js/BoardController.js
+++ js/BoardController.js
@@ -6,6 +6,7 @@ function BoardController(deps) {
 BoardController.prototype.attach = function(form) {
     var renderer = this.renderer;
     form.find('.board-column-cards').empty();
+    updateCounts(form);
 
     var repo = this.getBoardRepository();
     if (!repo) {
@@ -13,15 +14,29 @@ BoardController.prototype.attach = function(form) {
     }
 
     repo.getAll().then(function(rawCards) {
-        if (!rawCards) {
-            return;
-        }
-        for (var key in rawCards) {
-            if (rawCards.hasOwnProperty(key)) {
-                var card = new Card(rawCards[key]);
-                var column = form.find('.board-column[data-status="' + card.status + '"] .board-column-cards');
-                column.append(renderer.render(card));
+        if (rawCards) {
+            for (var key in rawCards) {
+                if (rawCards.hasOwnProperty(key)) {
+                    var card = new Card(rawCards[key]);
+                    var column = form.find('.board-column[data-status="' + card.status + '"] .board-column-cards');
+                    column.append(renderer.render(card));
+                }
             }
         }
+        updateCounts(form);
     });
 };
+
+function updateCounts(form) {
+    form.find('.board-column').each(function() {
+        var col = $(this);
+        var count = col.find('.board-column-cards .post-it').length;
+        var titleEl = col.find('.board-column-title');
+        var base = titleEl.data('base');
+        if (!base) {
+            base = titleEl.text();
+            titleEl.data('base', base);
+        }
+        titleEl.text(base + ' (' + count + ')');
+    });
+}
