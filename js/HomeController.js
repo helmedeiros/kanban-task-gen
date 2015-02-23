c2728b88f2b57b55fc14977d0bdea75fab01c451
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-02-23 19:15:00 -0200

    Drop dead authService handlers from HomeController

--- js/HomeController.js
+++ js/HomeController.js
@@ -1,24 +1,15 @@
 function HomeController(deps) {
-    this.authService = deps.authService;
     this.router = deps.router;
 }
 
 HomeController.prototype.attach = function(form) {
     var self = this;
 
-    form.on('submit', function(e) {
+    form.find('.home-cta').off('click.home').on('click.home', function(e) {
+        if (e.metaKey || e.ctrlKey || e.shiftKey) {
+            return;
+        }
         e.preventDefault();
-
-        var userAndPass = $(this).serializeObject();
-        userAndPass.password = '12345';
-
-        self.router.afterAuth(self.authService.signUpAndSignIn(userAndPass), 'gettingstarted');
-    });
-
-    form.find('.bt-social').on('click', function(e) {
-        e.preventDefault();
-
-        var provider = $(this).data('provider');
-        self.router.afterAuth(self.authService.signInWith(provider), 'gettingstarted');
+        self.router.routeTo('board');
     });
 };
